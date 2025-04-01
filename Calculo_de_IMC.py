0eb8587c742776c7a07579e9afbfacad06ee0dbe
Joseph Luis García Escobar <josephgarciaescobar597@gmail.com>
2025-03-31 19:57:12 -0500

    IMC

---
+++ Calculo_de_IMC.py
@@ -0,0 +1,33 @@
+
+while True:
+    try:
+        masa = input("Ingrese su peso (en kg): ")
+        masa = float(masa)
+        break
+    except ValueError:
+        print("Ingrese un caracter válido")
+
+
+while True:
+    try:
+        altura = input("Ingrese su altura (en metros): ")
+        altura = float(altura)
+        break
+    except ValueError:
+        print("Ingrese un caracter válido")
+
+
+
+IMC = masa / (altura)**2
+
+print(f"Su Índice de masa corporal es: {IMC: .2f}")
+
+if IMC < 18.5:
+    print("Está es bajo peso")
+elif IMC >= 18.5 and IMC <= 24.5:
+    print("Está en peso normal")
+elif IMC > 24.5 and IMC <= 29.9:
+    print("Está en sobrepeso")
+else:
+    print("Tiene obesidad")
+
